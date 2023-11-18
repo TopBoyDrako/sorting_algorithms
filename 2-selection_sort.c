81f8a47a54401e47791f6b0364858acbeaee303a
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-18 21:22:27 +0300

    refactor code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,30 +11,29 @@
 void selection_sort(int *array, size_t size)
 {
 	unsigned int i, j;
-	int min, min_id;
+	int min, min_index;
 	bool min_changed;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array[i];
+		min_index = i;
 		min_changed = false;
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (min > array[j])
+			if (array[min_index] > array[j])
 			{
-				min = array[j];
-				min_id = j;
+				min_index = j;
 				min_changed = true;
 			}
 		}
 		if (min_changed)
 		{
-			array[min_id] = array[i];
+			min = array[min_index];
+			array[min_index] = array[i];
 			array[i] = min;
 
 			print_array(array, size);
 		}
 	}
-
 }
